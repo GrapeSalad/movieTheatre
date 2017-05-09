9d3cf2fbabaf9ad12278f780c6756abef4f3907a
David Wilson and Jordan Loop <david+jordan@>
2017-05-09 15:10:32 -0700

    fix variable scope issue with click button appending content in tab.

---
+++ README.md
@@ -0,0 +1,9 @@
+management section: initialize Move constructor
+
+## SPECS
+
+* make tab work. smork smart
+* management portal pop up form for initializing a new movie object.
+* w/ button to create a new movie tab.
+* create movie class
+* AFTER LUNCH: populate the created tab with entered information: need to create template for end user form, html for entire tab div with appropriate id's and stuff, buttons for submission
--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,55 @@
 //BUSINESS LOGIC
+
+  // constructors
 function Movie(name, rating, synopsis) {
   this.movieName = name;
   this.movieRating = rating;
   this.movieSynopsis = synopsis;
-
 };
-
+function Theatre() {
+  this.theatreMovies = [];
+};
 
 //UI LOGIC
 $(document).ready(function() {
+  var newTheatre;
+
   $("#mgmtPortal").click(function(){
     $(".hidden-content").slideToggle();
   })
 
+  // mgmeForm submission collects movie data
   $("#mgmtForm").submit(function(event){
     event.preventDefault();
-    var mgmtMovieNameInput = $(this).find(".movieName").val();
+    var mgmtMovieNameInput = $(this).find(".movieName").val().replace(/\s/g, '');
     var mgmtMovieRatingInput = $(this).find(".movieRating").val();
     var mgmtMovieSynopsisInput =  $(this).find(".movieSynopsis").val();
 
+    // initialize newMovie object
     var newMovie = new Movie(mgmtMovieNameInput, mgmtMovieRatingInput, mgmtMovieSynopsisInput);
-
+    newTheatre = new Theatre(mgmtMovieNameInput);
+    newTheatre.theatreMovies.push(newMovie);
     console.log(newMovie);
+    console.log(newTheatre.theatreMovies[0].movieSynopsis);
+  });
+
 // Adds new tab
-    $("#addMovieToPage").last().click(function() {
-      $("ul.nav-tabs").append('<li class=""><a data-toggle="tab" href="#' +
-                          newMovie.movieName +
-                          '">' +
-                          newMovie.movieName +
-                          '</a></li>'
-      );
-    });
+  $("#addMovieToPage").last().click(function() {
+    console.log(newTheatre.theatreMovies);
+    $("ul#navTabs").append('<li class=""><a data-toggle="tab" href="#' +
+                        newTheatre.theatreMovies[0].movieName +
+                        '">' +
+                        newTheatre.theatreMovies[0].movieName +
+                        '</a></li>'
+    );
+
+    // append tab-content to tab-content-parent
+    $("div#tabContent").append('<div id="' +
+                                newTheatre.theatreMovies[0].movieName +
+                                '" class="tab-pane fade">' +
+                                '<p>'+ newTheatre.theatreMovies[0].movieRating +'</p>' +
+                                '<p>'+ newTheatre.theatreMovies[0].movieSynopsis +'</p>' +
+                                '</div>'
+    );
   });
 });
