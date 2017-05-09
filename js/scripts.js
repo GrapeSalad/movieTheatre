f9f2d76fbd7f944a1cfaa201d853e99c1be0fe05
David Wilson and Jordan Loop <david+jordan@>
2017-05-09 16:26:36 -0700

    Add variables to collect data from userForm and displays receipt.

--- js/scripts.js
+++ js/scripts.js
@@ -49,16 +49,28 @@ $(document).ready(function() {
                                 '<form class="userForm">' +
                                   '<label for="ticketType">Select a ticket type</label>'+
                                   '<select class="form-control" id="ticketType">'+
-                                    '<option>Standard</option>'+
-                                    '<option>Matine</option>'+
-                                    '<option>Seinor</option>'+
+                                    '<option value="8">Standard</option>'+
+                                    '<option value="7">Matine</option>'+
+                                    '<option value="7">Seinor</option>'+
                                   '</select>'+
                                   '<label for="ticketNumber">Quantity:</label>'+
                                   '<input type="number" id="ticketNumber">'+
                                   '<label for="userName">Name:</label>'+
                                   '<input type="text" id="userName" value="">'+
+                                  '<button type="submit" class="btn" name="userFormSubmit">Purchase</button>'+
                                 '</form>'+
                                 '</div>'
     );
+    //USER SUBMIT function
+    $("form.userForm").submit(function(event){
+      event.preventDefault();
+      var userTicketType = parseInt($("#ticketType").val());
+      var userTicketQuantity = parseInt($("#ticketNumber").val());
+      var userName = $("#userName").val();
+      var totalPrice = userTicketType * userTicketQuantity;
+
+      $("div#receipt").append("Thank you , " + userName + " for purchasing " + userTicketQuantity + " tickets to " + newTheatre.theatreMovies[0].movieName + " at the " + userTicketType + " rate.<br> Your total is $" + totalPrice + ".");
+    });
   });
+
 });
